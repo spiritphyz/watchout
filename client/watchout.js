77911b6908e9c16a36f09c1b084716f8582575c6
spiritphyz <heytonyle@icloud.com>
2016-09-24 17:19:51 -0700

    Implement basic collision detection
    -- Only works when the user is in the middle of a drag motion, not during stillness

--- client/watchout.js
+++ client/watchout.js
@@ -4,25 +4,21 @@ var gameOptions = {
   boardHeight: 900,
   gameInterval: 3000,
   reactionTime: 250,
-  numberOfEnemies: 10
+  numberOfEnemies: 1
 };
 
-var infoboard = {
+var infoBoard = {
   score: 0,
-  highscore: 0
-};
-
-var axes = {
-  x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.boardWidth]),
-  y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.boardHeight])
+  highscore: 0,
+  collisions: 0
 };
 
 var makeEnemies = function(amount = gameOptions.numberOfEnemies) {
   return _.range(0, amount).map(index => {
     return {
       id: index,
-      x: Math.random() * 100,
-      y: Math.random() * 100
+      x: Math.random() * gameOptions.boardWidth,
+      y: Math.random() * gameOptions.boardHeight
     };
   });
 };
@@ -30,8 +26,8 @@ var makeEnemies = function(amount = gameOptions.numberOfEnemies) {
 var makePlayer = function() {
   return [{
     id: 0,
-    x: 50,
-    y: 50
+    x: 0,
+    y: 0
   }];
 };
 
@@ -46,11 +42,37 @@ var dragstarted = function(d) {
 };
 
 var dragged = function(d) {
-  d3.select(this).attr('x', d.x = d3.event.x).attr('y', d.y = d3.event.y);
+  d3.select(this)
+    .attr('x', d.x = d3.event.x)
+    .attr('y', d.y = d3.event.y);
 };
 
 var dragended = function(d) {
   d3.select(this).classed('dragging', false);
+};
+
+
+var onCollision = function() {
+  updateBestScore();
+  infoBoard.score = 0;
+  infoBoard.collisions += 1;
+  updateScore();
+  updateCollisions();
+};
+
+var updateBestScore = function() {
+  infoBoard.highscore = _.max([infoBoard.highscore, infoBoard.score]);
+  d3.select('.highscore').text(infoBoard.highscore.toString());
+};
+
+var updateScore = function() {
+  d3.select('.current')
+    .text(infoBoard.score.toString());
+};
+
+var updateCollisions = function() {
+  d3.select('.collisions')
+    .text(infoBoard.collisions.toString());
 };
 
 var showBoard = function() {
@@ -64,24 +86,80 @@ var showEnemies = function(enemiesData) {
     .data(enemiesData, d => { 
       return d.id; 
     });
+  
+  var checkCollision = function(asteroid) {
+    // console.log('asteroid id: ', asteroid.id);
+    var player = d3.select('svg').selectAll('image').filter('.player');
+    var radiusSum = parseFloat(player[0][0].attributes.width.value);
+    var xDiff = parseFloat(asteroid.x) - parseFloat(player[0][0].attributes.x.value);
+    var yDiff = parseFloat(asteroid.y) - parseFloat(player[0][0].attributes.y.value);
+
+    // console.log('radiusSum', radiusSum);
+    // console.log('xDiff', xDiff);
+    // console.log('yDiff', yDiff);
+
+    var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    if (separation < radiusSum) {
+      console.log('collision occurred !!');
+      console.log('separation', separation);
+      console.log('asteroidx', asteroid.x);
+      console.log('asteroidy', asteroid.y);
+      console.log('playerx', player[0][0].attributes.x.value);
+      console.log('playerx', player[0][0].attributes.y.value);
+      onCollision();
+    }
+  };
+
+  var collisionTween = function(d, i) {
+    var enemy = d3.select(this);
+
+    var startPos = {
+      x: parseFloat(enemy.attr('x')),
+      y: parseFloat(enemy.attr('y'))
+    };
+
+    var endPos = {
+      x: d.x,
+      y: d.y
+    };
+
+    return function(t) {
+      checkCollision(d);
+
+      var asteroidNextPos = {
+        x: startPos.x + (endPos.x - startPos.x) * t,
+        y: startPos.y + (endPos.y - startPos.y) * t
+      };
+
+      enemy
+        .attr('x', asteroidNextPos.x)
+        .attr('y', asteroidNextPos.y);
+    };
+  };
 
   asteroids
-    .transition().duration(gameOptions.gameInterval - gameOptions.reactionTime)
-    .attr('x', enemy => axes.x(enemy.x))
-    .attr('y', enemy => axes.y(enemy.y));
+    .transition()
+      .duration(gameOptions.gameInterval - gameOptions.reactionTime)
+      .tween('custom', collisionTween)
+      .attr('x', enemy => enemy.x)
+      .attr('y', enemy => enemy.y);
+    // .transition()
+      // .duration(3000)
 
   asteroids.enter()
     .append('svg')
     .append('image')
     .attr('class', 'enemy')
     .attr('id', enemy => enemy.id)
-    .attr('x', enemy => axes.x(enemy.x))
-    .attr('y', enemy => axes.y(enemy.y))
+    .attr('x', enemy => enemy.x)
+    .attr('y', enemy => enemy.y)
     .attr('xlink:href', 'asteroid.png')
     .attr('height', '100px')
     .attr('width', '100px');
 
   asteroids.exit().remove();
+
+
 };
 
 var showPlayer = function(playerData) {
@@ -95,8 +173,8 @@ var showPlayer = function(playerData) {
     .append('image')
     .attr('class', 'player')
     .attr('id', player => player.id)
-    .attr('x', player => axes.x(player.x))
-    .attr('y', player => axes.y(player.y))
+    .attr('x', player => player.x)
+    .attr('y', player => player.y)
     .attr('xlink:href', 'astroboy.png')
     .attr('height', '150px')
     .attr('width', '150px')
@@ -112,10 +190,10 @@ var play = function() {
     showEnemies(newEnemyPositions);
   };
 
-  // var increaseScore = function() {
-  //   gameStats.score += 1;
-  //   updateScore();
-  // };
+  var increaseScore = function() {
+    infoBoard.score += 1;
+    updateScore();
+  };
 
   // Take a turn every 2 seconds
   showBoard();
@@ -123,10 +201,11 @@ var play = function() {
   showPlayer(newPlayerPosition);
   gameTurn();
   setInterval(gameTurn, gameOptions.gameInterval);
-
-  // Increment the score counter every 50ms
-  // setInterval(increaseScore, 50);
+  setInterval(increaseScore, 50);
 };
 
 // Play!
 play();
+
+
+
